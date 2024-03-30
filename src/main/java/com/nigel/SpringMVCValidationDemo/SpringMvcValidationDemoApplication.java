77101dd27333e89102e17eedfb92fa611b11f0cd
Nigel <mail@nigelmoes.nl>
2024-03-30 08:02:35 -0500

    Initial commit

---
+++ src/main/java/com/nigel/SpringMVCValidationDemo/SpringMvcValidationDemoApplication.java
@@ -0,0 +1,13 @@
+package com.nigel.SpringMVCValidationDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcValidationDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcValidationDemoApplication.class, args);
+	}
+
+}
